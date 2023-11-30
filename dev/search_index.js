530aba6c5d65004ad9d596449d6f577ecb2c15d4
Documenter.jl <documenter@juliadocs.github.io>
2023-11-30 11:24:43 +0000

    build based on b09cfa7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = maxnet","category":"page"},{"location":"#maxnet","page":"Home","title":"maxnet","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for maxnet.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [maxnet]","category":"page"}]
+}
