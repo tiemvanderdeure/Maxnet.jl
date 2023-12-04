706710f8acad5120ad92811122e59172adc40356
Michael Krabbe Borregaard <mkborregaard@sund.ku.dk>
2023-12-04 14:20:50 +0100

    add utility functions ported from R

--- src/maxnet.jl
+++ src/maxnet.jl
@@ -1,5 +1,9 @@
-module maxnet
+module MaxNet
+using Lasso
+using StatsModels
+using Missings
 
-# Write your package code here.
-
+include("utils.jl")
+include("terms.jl")
+include("maxnet_function.jl")
 end
