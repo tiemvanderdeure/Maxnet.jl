7cf9eca8279e5006d26c53c7dde3b261fd266cbd
Michael Krabbe Borregaard <mkborregaard@sund.ku.dk>
2023-12-04 14:22:44 +0100

    add a basic test (does not work yet)

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,14 @@ using Test
 @testset "maxnet.jl" begin
     # Write your tests here.
 end
+
+# Test stub based on the R package
+using DataFrames, CSV, CategoricalArrays
+bradypus = CSV.read("data/bradypus.tsv", DataFrame)
+bradypus.ecoreg = categorical(bradypus.ecoreg)
+p = bradypus.presence
+data = bradypus[:, 2:end]
+mod = maxnet(p, data)
+plot(mod, type = "cloglog")
+mod = amxnet(p, data, maxnet_formula(p, data, classes = "lq"))
+plot(mod, "tmp6190_ann")
