945479ef23ca89360f16e39b9b59fdbc1e172ee9
tiemvanderdeure <tiemvanderdeure@gmail.com>
2023-12-07 18:22:42 +0100

    initial implementation of maxnet()

--- src/Maxnet.jl
+++ src/Maxnet.jl
@@ -1,11 +1,18 @@
-module MaxNet
-using Lasso
-using StatsModels
-using Missings
+module Maxnet
+
+import Tables, Lasso, Interpolations
+import StatsModels, StatsAPI
+import StatsBase, Statistics
+
+using StatsModels: term
 
 # Write your package code here.
 
 include("utils.jl")
 include("terms.jl")
+include("feature_classes.jl")
+include("regularization.jl")
 include("maxnet_function.jl")
+include("data.jl")
+
 end
--- src/maxnet_function.jl
+++ src/maxnet_function.jl
@@ -1,31 +1,63 @@
 function addsamples(presences, predictors) # this should be a general sdm thing
-    ret = Int[]
-    for i in eachindex(presences)
-        if presences[i]
-            add = true
-            for j in eachindex(presences)
-                if (!presences[j]) && predictors[j, :] == predictors[i, :]
-                    add = false
-                    break
-                end
-            end
-            add && push!(ret, i)
-        end
-    end
-    vcat(presences, zeros(length(ret))), vcat(predictors, predictors[ret, :])
+    predictors = Tables.rowtable(predictors)
+    @assert length(presences) == length(predictors) 
+
+    to_add = setdiff(predictors[presences], predictors[.~presences]);
+
+    predictors_ = Tables.columntable([predictors; to_add])
+    presences_= vcat(presences, fill(false, length(to_add)))
+
+    return presences_, predictors_
 end
 
-function maxnet(presences, predictors, 
-                formula = maxnet_formula(presences, predictors),
-                regularization_multiplier = 1.0,
-                regularization_function = maxnet_default_regularization,
-                addsamplestobackground = true)
+function lambdas(reg, p, weights; λmax = 4, n = 200)
+    c = Statistics.mean(reg) * sum(p) / sum(weights)
+    10 .^ range(λmax, 0; length = n) .* c
+end
 
-    disallowmissing!(predictors)
+function maxnet(presences, predictors; features = [LinearFeature(), ProductFeature()],
+                regularization_multiplier = 1.0,
+                regularization_function = default_regularization,
+                addsamplestobackground = true, dofit = false)
+
+    #disallowmissing!(predictors)
     if addsamplestobackground
         presences, predictors = addsamples(presences, predictors)
+    else
+        predictors = Tables.columntable(predictors)
     end
 
-    #... TODO: This function is not yet finished
+    predictor_keys = keys(predictors)
+    terms = term.(predictor_keys)
+    continuous_indices = findall(predictor_keys .!= :ecoreg) # MLJ scitype system
 
+    # Get all the terms
+    all_terms = mapreduce(+, features) do fe
+        feature_terms(terms, continuous_indices, fe)
+    end
+
+    # Generate the model matrix
+    sch = StatsModels.schema(predictors, Dict(:ecoreg => StatsModels.FullDummyCoding()))
+    ts = StatsModels.apply_schema(all_terms, sch)
+    mm = StatsModels.modelcols(StatsModels.collect_matrix_terms(ts), predictors)
+
+    # Model matrix metadata
+    column_feature_classes = coef_features(ts)
+
+    # Generate regularization
+    reg = regularization_function(mm, column_feature_classes, presences)
+
+    # Generate weights
+    weights = presences .* 1. .+ (1 .- presences) .* 100.
+
+    # generate lambdas
+    λ = lambdas(reg, presences, weights; λmax = 4, n = 200)
+
+    machine = Lasso.fit(
+        Lasso.LassoPath, mm, presences, Lasso.Distributions.Binomial(); 
+        wts = weights, penalty_factor = reg, standardize = false, λ = λ,
+        dofit = dofit
+    )
+
+    return machine
 end
