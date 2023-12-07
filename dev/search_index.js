79a7a30a982e1b0cda239649c95e139108a8df3c
Documenter.jl <documenter@juliadocs.github.io>
2023-12-07 17:25:45 +0000

    build based on 945479e

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = maxnet","category":"page"},{"location":"#maxnet","page":"Home","title":"maxnet","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for maxnet.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [maxnet]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Maxnet","category":"page"},{"location":"#Maxnet","page":"Home","title":"Maxnet","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Maxnet.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Maxnet]","category":"page"}]
 }
