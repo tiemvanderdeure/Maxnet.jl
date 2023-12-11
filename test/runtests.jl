abed52cbce6b54b6a2d782b61aad66786f5bb79a
tiemvanderdeure <tiemvanderdeure@gmail.com>
2023-12-11 17:37:05 +0100

    add some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,53 @@
-using Maxnet
-using Test
+using Maxnet, Test, MLJBase
 
-@testset "Maxnet.jl" begin
-    # Write your tests here.
+p_a, env = Maxnet.bradypus()
+
+@testset "utils" begin
+    @test_throws ErrorException Maxnet.features_from_string("a")
 end
 
-# Test stub based on the R package
-using DataFrames, CSV, CategoricalArrays
-bradypus = CSV.read("data/bradypus.tsv", DataFrame)
-bradypus.ecoreg = categorical(bradypus.ecoreg)
-p = bradypus.presence
-data = bradypus[:, 2:end]
-mod = maxnet(p, data)
-plot(mod, type = "cloglog")
-mod = amxnet(p, data, maxnet_formula(p, data, classes = "lq"))
-plot(mod, "tmp6190_ann")
+@testset "Maxnet" begin
+    model_glmnet = Maxnet.maxnet(Bool.(p_a), env, "lq"; backend = GLMNetBackend());
+    model_lasso = Maxnet.maxnet(Bool.(p_a), env, "lq"; backend = LassoBackend());
+
+    pred_glmnet = Maxnet.predict(model_glmnet, env)
+    pred_lasso = Maxnet.predict(model_lasso, env)
+
+    # no classes
+    model_lasso = Maxnet.maxnet(Bool.(p_a), env; backend = LassoBackend());
+
+    # each class
+    Maxnet.maxnet(Bool.(p_a), env, "l"; backend = LassoBackend());
+    Maxnet.maxnet(Bool.(p_a), env, "q"; backend = LassoBackend());
+    Maxnet.maxnet(Bool.(p_a), env, "p"; backend = LassoBackend());
+    Maxnet.maxnet(Bool.(p_a), env, "h"; backend = LassoBackend());
+    Maxnet.maxnet(Bool.(p_a), env, "t"; backend = LassoBackend());
+
+    # some class combinations
+    Maxnet.maxnet(Bool.(p_a), env, "lq"; backend = LassoBackend());
+    Maxnet.maxnet(Bool.(p_a), env, "lqp"; backend = LassoBackend());
+    Maxnet.maxnet(Bool.(p_a), env, "lqh"; backend = LassoBackend());
+    Maxnet.maxnet(Bool.(p_a), env, "lqph"; backend = LassoBackend());
+    Maxnet.maxnet(Bool.(p_a), env, "lqpt"; backend = LassoBackend());
+end
+
+@testset "MLJ" begin
+    mn = Maxnet.MaxnetBinaryClassifier
+
+    # convert to continuous
+    cont_keys = collect(key => Continuous for key in keys(env) if key !== :ecoreg)
+    env_typed = MLJBase.coerce(env, cont_keys...)
+
+    # make a machine
+    mach1 = machine(mn(features = "lq", backend = LassoBackend()), env_typed, categorical(p_a))
+    fit!(mach1)
+    
+    mach2 = machine(mn(features = "lqph", backend = GLMNetBackend()), env_typed, categorical(p_a))
+    fit!(mach2)
+    
+    @test mach2.fitresult[1].path isa Maxnet.GLMNet.GLMNetPath
+
+    # predict via MLJBase
+    t = pdf.(MLJBase.predict(mach2, env_typed), true)
+end
+
