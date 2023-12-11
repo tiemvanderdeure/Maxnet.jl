96e768d5015173ee62079ac8ab9505ffccc5f746
tiemvanderdeure <tiemvanderdeure@gmail.com>
2023-12-11 17:36:05 +0100

    encode ecoreg as categorical

--- src/data.jl
+++ src/data.jl
@@ -2,6 +2,7 @@ using DelimitedFiles
 function bradypus() 
     data, headers = DelimitedFiles.readdlm("data/bradypus.tsv", '\t', Int; header = true)
     env = NamedTuple{Tuple(Symbol.(headers[2:end]))}(collect(eachcol(data)[2:end]))
+    env = merge(env, (; ecoreg = CategoricalArrays.categorical(env.ecoreg)))
     p_a = Bool.(data[:, 1])
     return p_a, env
 end
