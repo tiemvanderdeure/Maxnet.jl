109ff0669bab214686201beb227916be79a59ba7
tiemvanderdeure <tiemvanderdeure@gmail.com>
2023-12-21 15:52:05 +0100

    add function to get variable keys from column

--- src/model_matrix.jl
+++ src/model_matrix.jl
@@ -7,27 +7,7 @@ end
 
 ModelMatrixColumn(feature, key, args...) = ModelMatrixColumn(feature, key, args)
 
-# Generate the actual column from the data and the column specification
-_get_column(data, ::LinearFeature, key) = data[key]
-_get_column(data, ::CategoricalFeature, key, x) = data[key] .== x
-_get_column(data, ::QuadraticFeature, key) = data[key].^2
-_get_column(data, ::ProductFeature, key, key2) = data[key] .* data[key2]
-_get_column(data, ::HingeFeature, key, mi, ma) = hingeval.(data[key], mi, ma)
-_get_column(data, ::ThresholdFeature, key, x) = data[key] .>= x
-
-_get_column(data, c::ModelMatrixColumn) = _get_column(data, c.feature, c.key, c.args...)
-
-function _model_matrix(data, cols::Vector{<:ModelMatrixColumn})
-    # pre-allocate memory
-    A = zeros(Float64, length(first(data)), length(cols))
-
-    for (i, c) in enumerate(cols)
-        A[:, i] .= _get_column(data, c)
-    end
-    return A
-end
-    #mapreduce(c -> _get_column(data, c), hcat, cols)
-
+ # Get ModelMatrixColumn objects from each feature, given some input data
 _feature_columns(cont_vars, cat_vars, f::LinearFeature, nk) = [ModelMatrixColumn(f, k) for k in keys(cont_vars)]
 _feature_columns(cont_vars, cat_vars, f::QuadraticFeature, nk) = [ModelMatrixColumn(f, k) for k in keys(cont_vars)]
 function _feature_columns(cont_vars, cat_vars, f::CategoricalFeature, nk)
@@ -57,5 +37,33 @@ function _feature_columns(cont_vars, cat_vars, f::ThresholdFeature, nk)
     mapreduce(vcat, keys(cont_vars)) do k
         thresholds = range(extrema(cont_vars[k])...; length = nk + 2)[2:nk + 1]
         [ModelMatrixColumn(f, k, t) for t in thresholds]
+    end
+end
+
+# Generate the actual column from the data and the column specification
+_get_column(data, ::LinearFeature, key) = data[key]
+_get_column(data, ::CategoricalFeature, key, x) = data[key] .== x
+_get_column(data, ::QuadraticFeature, key) = data[key].^2
+_get_column(data, ::ProductFeature, key, key2) = data[key] .* data[key2]
+_get_column(data, ::HingeFeature, key, mi, ma) = hingeval.(data[key], mi, ma)
+_get_column(data, ::ThresholdFeature, key, x) = data[key] .>= x
+
+_get_column(data, c::ModelMatrixColumn) = _get_column(data, c.feature, c.key, c.args...)
+
+function _model_matrix(data, cols::Vector{<:ModelMatrixColumn})
+    # pre-allocate memory
+    A = zeros(Float64, length(first(data)), length(cols))
+
+    for (i, c) in enumerate(cols)
+        A[:, i] .= _get_column(data, c)
+    end
+    return A
+end
+
+function _var_keys(c::ModelMatrixColumn)
+    if c.feature == ProductFeature()
+        [c.key, c.args[1]]
+    else
+        [c.key]
     end
 end
