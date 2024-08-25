42d8a99a681c3db3cb49e6fd013f8a02d6d80252
tiemvanderdeure <tiemvanderdeure@gmail.com>
2024-08-25 14:51:34 +0200

    actually pass keywords to glmnet

--- src/lasso.jl
+++ src/lasso.jl
@@ -1,11 +1,11 @@
 function fit_lasso_path(
     mm, presences;
-    wts, penalty_factor, λ, kw...) 
+    weights, penalty_factor, lambda, kw...) 
 
     presence_matrix = [1 .- presences presences]
     GLMNet.glmnet(
         mm, presence_matrix, GLMNet.Binomial(); 
-        weights = wts, penalty_factor = penalty_factor, lambda = λ, standardize = false)
+        weights, penalty_factor, lambda, standardize = false, kw...)
 end
 
 get_coefs(path::GLMNet.GLMNetPath) = path.betas
