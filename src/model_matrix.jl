e94f042ac7691a02c666e71191e7b8181218c6ba
tiemvanderdeure <tiemvanderdeure@gmail.com>
2026-09-22 09:55:23 +0200

    simplify feature_columns logic - replace hcat with vcat

--- src/model_matrix.jl
+++ src/model_matrix.jl
@@ -1,4 +1,4 @@
-# A type with all metadata needed to (re)construct a column from data
+# A type with all metadata needed to (re)construct a column from data - but not actual data
 struct ModelMatrixColumn{F<:AbstractFeatureClass}
     feature::F
     key::Symbol
@@ -7,15 +7,16 @@ end
 
 ModelMatrixColumn(feature, key, args...) = ModelMatrixColumn(feature, key, args)
 
- # Get ModelMatrixColumn objects from each feature, given some input data
-_feature_columns(cont_vars, cat_vars, f::LinearFeature, nk) = [ModelMatrixColumn(f, k) for k in keys(cont_vars)]
-_feature_columns(cont_vars, cat_vars, f::QuadraticFeature, nk) = [ModelMatrixColumn(f, k) for k in keys(cont_vars)]
+### Get ModelMatrixColumn objects from each feature, given some input data
+# fallback method - get columns one continuous variable at a time and vcat
+_feature_columns(cont_vars, cat_vars, f, nk) = mapreduce(k -> __columns(f, k, cont_vars[k], nk), vcat, keys(cont_vars))
+# for categorical variable map over categorical variables instead
 function _feature_columns(cont_vars, cat_vars, f::CategoricalFeature, nk)
-    mapreduce(hcat, keys(cat_vars)) do k
+    mapreduce(vcat, keys(cat_vars)) do k
         [ModelMatrixColumn(f, k, (x,)) for x in CategoricalArrays.levels(cat_vars[k])]
     end
 end
-
+# product needs its own implementation as it combines variables
 function _feature_columns(cont_vars, cat_vars, f::ProductFeature, nk)
     ks = keys(cont_vars)
     n = length(cont_vars)
@@ -25,20 +26,10 @@ function _feature_columns(cont_vars, cat_vars, f::ProductFeature, nk)
         end
     end
 end
-   
-function _feature_columns(cont_vars, cat_vars, f::HingeFeature, nk)
-    mapreduce(vcat, keys(cont_vars)) do k
-        ranges = hinge_ranges(cont_vars[k], nk)
-        [ModelMatrixColumn(f, k, r) for r in ranges]
-    end
-end
-
-function _feature_columns(cont_vars, cat_vars, f::ThresholdFeature, nk)
-    mapreduce(vcat, keys(cont_vars)) do k
-        thresholds = range(extrema(cont_vars[k])...; length = nk + 2)[2:nk + 1]
-        [ModelMatrixColumn(f, k, t) for t in thresholds]
-    end
-end
+__columns(f, k, args...) = ModelMatrixColumn(f, k) # fallback method for linear and quadratic
+__columns(f::HingeFeature, k, var, nk) = [ModelMatrixColumn(f, k, r) for r in hinge_ranges(var, nk)]
+__columns(f::ThresholdFeature, k, var, nk) = 
+    [ModelMatrixColumn(f, k, t) for t in range(extrema(var)...; length = nk + 2)[2:nk + 1]]
 
 # Generate the actual column from the data and the column specification
 _get_column!(A, data, ::LinearFeature, key) = A .= data[key]
@@ -46,7 +37,7 @@ _get_column!(A, data, ::CategoricalFeature, key, x) = A .= data[key] .== (x)
 _get_column!(A, data, ::QuadraticFeature, key) = A .= data[key].^2
 _get_column!(A, data, ::ProductFeature, key, key2) = A .= data[key] .* data[key2]
 _get_column!(A, data, ::HingeFeature, key, mi, ma) = A .= hingeval.(data[key], mi, ma)
-_get_column!(A, data, ::ThresholdFeature, key, x) = A .= A .= data[key] .>= x
+_get_column!(A, data, ::ThresholdFeature, key, x) = A .= data[key] .>= x
 
 function _get_column!(A, data, c::ModelMatrixColumn)
     _get_column!(A, data, c.feature, c.key, c.args...)
